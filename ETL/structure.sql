043e402264e959fac8a1ec4af40d53e223f97e6c
alexdjk01 <ionelalexandru01@gmail.com>
2025-06-26 10:34:35 +0300

    Chnaged structure comments

--- ETL/structure.sql
+++ ETL/structure.sql
@@ -1,5 +1,4 @@
 /*
-    Author: Ionel Miha-Alexandru DAVAX BHD
     This script is in charge with overall project structure - eg. tables.
     Target: Create a complex galaxy(star) schema for DAVAX employees, taking into consideration all the information avaiable.
     
